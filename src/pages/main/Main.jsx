39a3efc1d0c9a4d8e68a13ac6f19e082ae4b5b38
Marina Filippova <marisha.mi0@ya.ru>
2025-04-28 10:09:15 +0300

    add pagination

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -5,48 +5,105 @@ import { getNews } from "../../api/apiNews";
 
 import NewsList from "../../component/newsList/NewsList";
 import Skeleton from "../../component/skeleton/Skeleton";
-import NewsBanner from '../../component/newsBanner/NewsBanner';
+import NewsBanner from "../../component/newsBanner/NewsBanner";
+import { mockNews } from "./mock";
+import Pagination from "../../component/pagination/Pagination";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
+
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true);
+      const response = await getNews(currentPage, pageSize);
+      setNews(response.news);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true);
-        const response = await getNews();
-        setNews(response.news);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
 
-   // Найти первую новость с валидной картинкой
-  const bannerItem = news.find(item => item.image && item.image !== "None");
-    // Отфильтровываем список новостей без баннер-новости
-    const filteredNews = bannerItem 
-    ? news.filter(item => item.id !== bannerItem.id)
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // let news = mockNews;
+  // Найти первую новость с валидной картинкой
+  const bannerItem = news.find((item) => item.image && item.image !== "None");
+  // Отфильтровываем список новостей без баннер-новости
+  const filteredNews = bannerItem
+    ? news.filter((item) => item.id !== bannerItem.id)
     : news;
 
   return (
     <main className={styles.main}>
-      {bannerItem  && !isLoading ? (
+      {bannerItem && !isLoading ? (
         <NewsBanner item={bannerItem} />
       ) : (
         <Skeleton type={"banner"} count={1} />
       )}
+
+      <Pagination
+        handlePageClick={handlePageClick}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
 
       {!isLoading ? (
         <NewsList news={filteredNews} />
       ) : (
         <Skeleton type={"item"} count={10} />
       )}
+
+      <Pagination
+        handlePageClick={handlePageClick}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </main>
   );
 };
+
+//для работы в mock тк кол-во запросов ограничено
+//   return (
+//     <main className={styles.main}>
+//       {bannerItem ? (
+//         <NewsBanner item={bannerItem} />
+//       ) : (
+//         <Skeleton type={"banner"} count={1} />
+//       )}
+
+//       {filteredNews.length > 0 ? (
+//         <NewsList news={filteredNews} />
+//       ) : (
+//         <Skeleton type={"item"} count={10} />
+//       )}
+//     </main>
+//   );
+// };
 
 export default Main;
