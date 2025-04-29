36dc04a9ef1a0b6f83372e86147b7d9c289a883d
Marina Filippova <marisha.mi0@ya.ru>
2025-04-29 12:10:25 +0300

    add search input and debounce hook

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,12 +3,15 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_API_KEY;
 const url = process.env.REACT_APP_BASE_URL;
 
-export const getNews = async (
-  { page_number = 1, page_size = 10, category } 
-) => {
+export const getNews = async ({
+  page_number = 1,
+  page_size = 10,
+  category,
+  keywords,
+}) => {
   try {
     const response = await axios.get(`${url}search`, {
-      params: { apiKey: apiKey, page_number, page_size, category },
+      params: { apiKey: apiKey, page_number, page_size, category, keywords },
     });
 
     return response.data;
